122ab2107192941cabe545868e2bd5f830d08c23
triangletrianglesquareplusx <preslavgetov@gmail.com>
2024-02-05 12:11:59 +0200

    adding artist interface and adding artist attr

--- src/app/_components/post-header.tsx
+++ src/app/_components/post-header.tsx
@@ -3,15 +3,16 @@ import CoverImage from "./cover-image";
 import DateFormatter from "./date-formatter";
 import { PostTitle } from "@/app/_components/post-title";
 import { type Author } from "@/interfaces/author";
-
+import {type Artist} from "@/interfaces/artist"
 type Props = {
   title: string;
   coverImage: string;
   date: string;
   author: Author;
+  artistAttr?: Artist
 };
 
-export function PostHeader({ title, coverImage, date, author }: Props) {
+export function PostHeader({ title, coverImage, date, author, artistAttr }: Props) {
   return (
     <>
       <PostTitle>{title}</PostTitle>
@@ -19,7 +20,7 @@ export function PostHeader({ title, coverImage, date, author }: Props) {
         <Avatar name={author.name} picture={author.picture} />
       </div>
       <div className="mb-8 md:mb-16 sm:mx-0">
-        <CoverImage title={title} src={coverImage} />
+        <CoverImage title={title} src={coverImage} artistName={artistAttr?.name} artistUrl={artistAttr?.url}/>
       </div>
       <div className="max-w-2xl mx-auto">
         <div className="block md:hidden mb-6">
